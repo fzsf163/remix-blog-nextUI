56599efce1ded093309458166a29d6edc1b090d3
fzsf163 <yofaisol619@gmail.com>
2024-07-02 05:40:25 +0600

    sub responsive

--- app/components/subscribe/subscribe.tsx
+++ app/components/subscribe/subscribe.tsx
@@ -3,21 +3,23 @@ import "./subscribe.css";
 import { IconMailFilled } from "@tabler/icons-react";
 export default function SubscribeBox() {
   return (
-    <div className="text-center space-y-4 bg-slate-400/30 py-12">
-      <h1 className="get-notified fa-text text-sm font-extrabold capitalize sm:text-xl md:text-2xl lg:text-4xl xl:text-6xl">
+    <div className="space-y-4 bg-slate-400/30 py-12 text-center">
+      <h1 className="get-notified fa-text text-sm font-extrabold capitalize sm:text-5xl xl:text-6xl">
         Get notified with new articles
       </h1>
-      <div className="m-auto flex w-fit items-center justify-center gap-3">
+      <div className="m-auto flex w-fit flex-col items-center justify-center gap-3 sm:flex-row">
         <div className="group flex items-center justify-center rounded-md bg-[#6894b1]">
-          <IconMailFilled className="h-[80px] w-[50px] pl-2 text-[#93b4ca] duration-250 ease-in-out transition-all group-focus-within:w-[0px] group-focus-within:pl-0" />
+          <IconMailFilled className="pl-2 text-[#93b4ca] transition-all duration-250 ease-in-out group-focus-within:w-[0px] group-focus-within:pl-0 sm:h-[80px] sm:w-[50px]" />
           <input
             placeholder="your@email.com"
-            className="input h-[80px] w-[400px]"
+            className="input placeholder:text-sm placeholder:text-white/30 sm:h-[80px] sm:w-[400px] sm:placeholder:text-xl"
             name="emailHomePageSub"
             type="email"
           />
         </div>
-        <button className="buttonSub h-[80px] w-[200px]">Subscribe</button>
+        <button className="buttonSub sm:h-[80px] sm:w-[200px]">
+          Subscribe
+        </button>
       </div>
     </div>
   );
