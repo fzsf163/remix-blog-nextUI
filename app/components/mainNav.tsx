d1380997514b338e2a9502563a46380fac682c69
fzsf163 <yofaisol619@gmail.com>
2024-07-03 05:40:38 +0600

    some fix

--- app/components/mainNav.tsx
+++ app/components/mainNav.tsx
@@ -1,6 +1,6 @@
 import { Link, useLocation, useNavigate } from "@remix-run/react";
 import { motion } from "framer-motion";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import {
   IconHomeFilled,
   IconLayoutGridFilled,
@@ -14,17 +14,17 @@ const navItems = [
     icon: <IconHomeFilled></IconHomeFilled>,
   },
   {
-    path: "/blogs",
+    path: "blogs",
     name: "Blogs",
     icon: <IconArticleFilled></IconArticleFilled>,
   },
   {
-    path: "/about",
+    path: "about",
     name: "About",
     icon: <IconLayoutGridFilled></IconLayoutGridFilled>,
   },
   {
-    path: "/request",
+    path: "request",
     name: "Request",
     icon: <IconDialpadFilled></IconDialpadFilled>,
   },
@@ -32,23 +32,41 @@ const navItems = [
 
 export default function NavItems() {
   let location = useLocation();
-  let pathname = "" ?? "/";
+  let patharray = location.pathname.split("/");
+
+  let pathname = "/";
 
   if (location.pathname) {
-    pathname = location.pathname;
+    if (patharray[1] === "") {
+      pathname = "/";
+    } else {
+      pathname = patharray[1];
+    }
   }
 
   const [hoveredPath, setHoveredPath] = useState(pathname);
+  const [activeNow, setActiveNow] = useState(pathname);
+  // useEffect(() => {
+  //   if (location.pathname) {
+  //     if (patharray[1] === "") {
+  //       setActiveNow("/");
+  //       // setHoveredPath(patharray[1]);
+  //     } else {
+  //       setActiveNow(patharray[1]);
+  //       // setHoveredPath("/");
+  //     }
+  //   }
+  // }, [patharray]);
 
   return (
-    <div className="hidden md:block   p-[0.4rem] rounded-md  z-[100] bg-slate-700/20 backdrop-blur-md text-black">
-      <nav className="flex gap-2 relative justify-start w-full z-[100]  rounded-lg">
+    <div className="z-[100] hidden rounded-md bg-slate-700/20 p-[0.4rem] text-black backdrop-blur-md md:block">
+      <nav className="relative z-[100] flex w-full justify-start gap-2 rounded-lg">
         {navItems.map((item, index) => {
           const isActive = item.path === pathname;
           return (
             <Link
               key={item.path}
-              className={`px-4 py-2 rounded-md text-sm lg:text-base relative no-underline duration-300 ease-in ${
+              className={`relative rounded-md px-4 py-2 text-sm no-underline duration-300 ease-in lg:text-base ${
                 isActive
                   ? "text-zinc-100"
                   : "light:text-black dark:text-white/70"
@@ -58,20 +76,20 @@ export default function NavItems() {
               onMouseOver={() => setHoveredPath(item.path)}
               onMouseLeave={() => setHoveredPath(pathname)}
             >
-              <span className="flex justify-center items-center gap-2 mix-blend-exclusion hover:text-white transition-all duration-100 ease-in-out">
+              <span className="flex items-center justify-center gap-2 mix-blend-exclusion transition-all duration-100 ease-in-out hover:text-white">
                 {" "}
                 {item.icon} {item.name}
               </span>
               {item.path === hoveredPath && (
                 <motion.div
-                  className="absolute bottom-0 left-0 h-full bg-stone-800/90 rounded-md -z-10 dark:bg-zinc-200"
+                  className="absolute bottom-0 left-0 -z-10 h-full rounded-md bg-stone-800/90 dark:bg-zinc-200"
                   layoutId="navbar"
                   aria-hidden="true"
                   style={{
                     width: "100%",
                   }}
                   transition={{
-                    type: "spring",
+                    type: "tween",
                     bounce: 0.1,
                     stiffness: 130,
                     damping: 9,
