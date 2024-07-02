0af3fbeb2d3ff5b4f974083f035d7fd1c5d36b91
fzsf163 <yofaisol619@gmail.com>
2024-07-03 04:46:00 +0600

    done home page

--- app/components/trending-posts/trending-post.tsx
+++ app/components/trending-posts/trending-post.tsx
@@ -36,57 +36,46 @@ const trending = [
 ];
 export default function TrendingPost() {
   return (
-    <div className="m-auto text-center">
+    <div className="m-auto text-center space-y-10">
       <h1 className="fa-text text-xl font-bold uppercase sm:text-2xl md:text-5xl lg:text-6xl xl:text-8xl 2xl:text-9xl">
         Trending Posts
       </h1>
 
-      <div className="row-span-2 mt-10 inline-grid w-fit grid-cols-12 rounded-xl bg-slate-300 p-10 shadow-md sm:gap-10">
+      <div className=" inline-grid w-fit grid-cols-12 gap-4 bg-slate-300 p-2 shadow-md sm:gap-10 sm:rounded-xl md:p-20">
         {trending.map((trend, index) => {
           return (
             <Tooltip
               key={index + 1}
               content="See Post"
-              showArrow
-              className="rounded-md"
+              className="bg-black text-white"
             >
               <div
                 key={index}
-                className="col-span-12 flex w-fit cursor-pointer justify-center gap-4 rounded-xl p-3 shadow-lg transition-colors duration-400 ease-in-out hover:text-white light:bg-blue-200 hover:light:bg-blue-400 dark:bg-slate-500 hover:dark:bg-slate-400 lg:col-span-6 xl:items-start"
+                className="col-span-12 flex h-[7rem] md:h-[9rem] w-fit cursor-pointer justify-center gap-0  rounded-xl shadow-lg transition-colors duration-400 ease-in-out light hover:text-white light:bg-blue-400/40 hover:light:bg-blue-400 dark:bg-slate-500 hover:dark:bg-slate-400 lg:col-span-6 xl:items-start "
               >
-                <Image
-                  removeWrapper
-                  alt="Card background"
-                  className="hidden rounded-lg object-cover sm:block"
-                  src={trend.img}
-                  width={200}
-                />
-                <Image
-                  removeWrapper
-                  alt="Card background"
-                  className="rounded-lg object-cover sm:hidden"
-                  src={trend.img}
-                  width={100}
-                />
-                <div className="flex h-full max-w-[400px] flex-col items-start gap-4 pt-3">
-                  <h1 className="md:text-md text-start text-xs font-bold capitalize sm:text-sm lg:text-lg xl:text-2xl">
+                <div className="group aspect-auto w-28 overflow-hidden rounded-lg p-2 sm:w-52">
+                  <img
+                    src={trend.img}
+                    className="h-full w-full rounded-lg transition-transform duration-500 ease-soft-spring group-hover:scale-125"
+                  ></img>
+                </div>
+                <div className="flex h-full max-w-[400px] flex-col items-start justify-between gap-1 p-5">
+                  <h1 className="md:text-lg text-start text-xs font-bold capitalize sm:text-sm lg:text-lg xl:text-2xl">
                     {trend.title}
                   </h1>
-                  <div className="flex items-start justify-between sm:gap-10">
-                    <Button
-                      isDisabled
-                      startContent={<IconCalendarEvent stroke={2} />}
-                      className="bg-transparent p-0 text-xs sm:text-sm md:text-lg"
-                    >
-                      {trend.published}
-                    </Button>
-                    <Button
-                      isDisabled
-                      startContent={<IconBook2 stroke={2} />}
-                      className="bg-transparent p-0 text-xs sm:text-sm md:text-lg"
-                    >
-                      {trend.readingTime}
-                    </Button>
+                  <div className="flex items-start justify-between gap-4 sm:gap-10 *:[&_button]:light:text-black/30 *:[&_button]:dark:text-white/30">
+                    <button className="flex items-center justify-between bg-transparent p-0 text-xs sm:text-sm md:text-lg">
+                      <span>
+                        <IconCalendarEvent stroke={2} />
+                      </span>
+                      <p> {trend.published}</p>
+                    </button>
+                    <button className="pointer-events-none flex items-center justify-between bg-transparent p-0 text-xs sm:text-sm md:text-lg">
+                      <span>
+                        <IconBook2 stroke={2} />
+                      </span>
+                      <p> {trend.readingTime}</p>
+                    </button>
                   </div>
                 </div>
               </div>
